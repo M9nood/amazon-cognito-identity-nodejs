9ef3f8b29402332addddbfd5532070dd6290f206
m9nood <nattawoot@livingmobile.me>
2021-02-12 17:11:18 +0700

    feat : refresh token and sign out

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,7 @@ const AuthController = require('../controllers/auth-controller')
 router.post('/signin', AuthController.signInHandler)
 router.post('/signup', AuthController.signUpHandler)
 router.post('/verify-code', AuthController.confirmRegistationHandler)
+router.post('/tokens/refresh', AuthController.refreshTokenHandler)
+router.post('/signout', AuthController.signOutHandler)
 
 module.exports = router
